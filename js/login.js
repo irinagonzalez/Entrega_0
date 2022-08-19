8311406dab3329b4ef22ba8f53f8e711743ca3e5
irinagonzalez <irinagonzalez@users.noreply.github.com>
2022-08-19 01:01:56 -0300

    modificado

--- js/login.js
+++ js/login.js
@@ -1,14 +1,18 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        window.location = "products.html"
-    });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html"
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html"
-    });
+document.addEventListener ("DOMContentLoaded", function() {
+    let username = document.getElementById("inputusername").value;
+    let password = document.getElementById("inputpassword").value;
+    let correcto = true;
+    
+    if (username == "") {
+        alert ("Completar informacion solicitada");
+        correcto = false;
+    }
+    if (password == "") {
+        alert ("Completar informacion solicitada");
+        correcto = false;
+    }
+
+    if (correcto) {
+        window.location.href= "index.html"
+    } 
 });
